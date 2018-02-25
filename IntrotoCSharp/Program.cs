99811571ad02e2794339cafc93792cf095f14846
madasuvamsi <madasuvamsi@gmail.com>
2018-02-25 11:46:47 -0600

    Add project files.

---
+++ IntrotoCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IntrotoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome to C# Programming");
+                Console.ReadKey();
+            }
+
+            catch
+            {
+                Console.WriteLine("Error Occured");
+            }
+        }
+    }
+}

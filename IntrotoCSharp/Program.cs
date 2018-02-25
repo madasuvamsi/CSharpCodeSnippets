675c6bec67c86c5333acf234a5d5b2642202d031
madasuvamsi <madasuvamsi@gmail.com>
2018-02-25 12:10:26 -0600

    added int a variable

--- IntrotoCSharp/Program.cs
+++ IntrotoCSharp/Program.cs
@@ -9,6 +9,7 @@ namespace IntrotoCSharp
             try
             {
                 int i = 10;
+                int a = 20;
                 Console.WriteLine("Welcome to C# Programming");
                 Console.ReadKey();
 

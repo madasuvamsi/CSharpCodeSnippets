c5ebc79f54fdbb4ce6923bfb3b29df5e87152b89
madasuvamsi <madasuvamsi@gmail.com>
2018-02-25 13:10:22 -0600

    added a addition class

---
+++ IntrotoCSharp/AdditionClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntrotoCSharp
+{
+    class AdditionClass
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 30;
+            int c;
+            c = a + b;
+            Console.WriteLine("The value of C is :{0}", c);
+            Console.ReadKey();
+        }
+        
+    
+    }
+}

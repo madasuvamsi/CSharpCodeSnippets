30c28cb78b1c9cab27e52f3ab31aec7d5e6493ee
madasuvamsi <madasuvamsi@gmail.com>
2018-02-25 13:44:52 -0600

    added reading and writing class

---
+++ IntrotoCSharp/ReadingandWritin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntrotoCSharp
+{
+    class ReadingandWritin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Username:");
+            string username = Console.ReadLine();
+            Console.WriteLine("Hello {0}", username);
+            Console.ReadKey();
+        }
+    }
+}
